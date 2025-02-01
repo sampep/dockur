27ba474c6676e7db3a3c452d55864d3685cb9fd8
sampep <153245783+sampep@users.noreply.github.com>
2025-02-01 06:23:52 +0100

    ok

--- bash.sh
+++ bash.sh
@@ -2,5 +2,5 @@
 
 while true; do
     echo "$(date '+%Y-%m-%d %H:%M:%S')"
-    sleep 600  # Wait for 600 seconds (10 minutes) before printing again
+    sleep 300  # Wait for 300 seconds (5 minutes) before printing again
 done
